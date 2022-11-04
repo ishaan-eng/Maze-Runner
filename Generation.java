661b93ed49a2e5be3aa8b3673922305335ca484e
ishaan-eng <70952744+ishaan-eng@users.noreply.github.com>
2022-11-04 18:53:46 +0530

    Update Generation.java

--- Generation.java
+++ Generation.java
@@ -3,13 +3,13 @@ package maze.generation;
 import static java.util.stream.IntStream.range;
 
 public class DisjointSet
-{
-
-    int[] parent;
-    int[] rank;
-    int size;
+{                                //Representatives for disjoint subsets. If the set consists
+                                 //only of the one element its parent equals to its id.
+    int[] parent;                //Otherwise, its parent is the next element up the tree.
+    int[] rank;                  //Heights of the trees corresponding to the subsets. A subset with one element has a rank of zero.
+    int size;                    //The number of disjoint subsets.
     
-    public DisjointSet(int size)
+    public DisjointSet(int size)                //Constructs a disjoint set of disjoint subsets(size).
     {
         this.size = size;
         parent = new int[size];
@@ -17,25 +17,25 @@ public class DisjointSet
         range(0, size).forEach(this::makeSet);
     }
 
-    void makeSet(int i) 
+    void makeSet(int i)                         //Initializes a particular set.
     {
         parent[i] = i;
         rank[i] = 0;
     }
 
-    public int getSize() 
+    public int getSize()                        //Returns the number of disjoint subsets.
     {
         return size;
     }
 
-    public int find(int i) 
+    public int find(int i)                      //Finds a representative for the set. If the set consists only of the one element its parent equals to its id.
     {
         if (i != parent[i])
             parent[i] = find(parent[i]);
         return parent[i];
     }
 
-    public boolean union(int i, int j) 
+    public boolean union(int i, int j)          //Merges two disjoint sets into one by the ids if their ids are not in the same set already.
     {
         var iRoot = find(i);
         var jRoot = find(j);
